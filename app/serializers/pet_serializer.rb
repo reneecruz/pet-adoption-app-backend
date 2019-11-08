f6b79112975dfe95ff2f4608a1eb250e3d8ccb85
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2019-11-07 19:27:58 -0500

    Serializer is fixed

--- app/serializers/pet_serializer.rb
+++ app/serializers/pet_serializer.rb
@@ -1,6 +1,6 @@
 class PetSerializer < ActiveModel::Serializer
   attributes :id, :name, :age, :species, :location
 
-  has_one: adoption 
-  has_one: user
+  has_one :adoption 
+  has_one :user
 end
